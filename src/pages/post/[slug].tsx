f65ecd4bb4123f40a9d56172b8323d68d59e2bc0
Daiane Sousa <daisousa.c@gmail.com>
2021-09-18 12:13:31 -0300

    add page post

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,6 +1,10 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import { FiCalendar, FiUser, FiClock } from 'react-icons/fi';
-import { format } from 'date-fns';
+import { format, parseISO } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+import { useRouter } from 'next/router';
 import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
@@ -29,63 +33,75 @@ interface PostProps {
   post: Post;
 }
 
-export default function Post(): JSX.Element {
+export default function Post({ post }: PostProps): JSX.Element {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Carregando...</div>;
+  }
+
+  const amountWordsOfBody = RichText.asText(
+    post.data.content.reduce((acc, data) => [...acc, ...data.body], [])
+  ).split(' ').length;
+
+  const amountWordsOfHeading = post.data.content.reduce((acc, data) => {
+    if (data.heading) {
+      return [...acc, ...data.heading.split(' ')];
+    }
+
+    return [...acc];
+  }, []).length;
+
+  function dateFormated(date: string) {
+    return format(parseISO(date), 'd MMM yyyy', { locale: ptBR });
+  }
+
+  const readingTime = Math.ceil(
+    (amountWordsOfBody + amountWordsOfHeading) / 200
+  );
+
   return (
     <>
       <div className={commonStyles.container}>
         <Header />
       </div>
       <div className={styles.banner}>
-        <img src="/images/banner.jpg" alt="Banner" />
+        {post.data?.banner?.url && (
+          <img src={post.data.banner.url} alt="banner" />
+        )}
       </div>
       <div className={commonStyles.container}>
         <main className={styles.postContainer}>
-          <div>
-            <h1>Criando um app CRA do zero</h1>
-            <p className={styles.infoContent}>
+          <div className={styles.infoContent}>
+            <h1>{post.data.title}</h1>
+            <p className={styles.infoDetails}>
               <span>
                 <FiCalendar size={20} color="#BBBBBB" />
-                15 Mar 2021
+                {dateFormated(post.first_publication_date)}
               </span>
               <span>
                 <FiUser size={20} color="#BBBBBB" />
-                Daiane Sousa
+                {post.data.author}
               </span>
               <span>
                 <FiClock size={20} color="#BBBBBB" />
-                10 min
+                {readingTime} min
               </span>
             </p>
           </div>
 
-          <article>
-            <h2>Proin et varius</h2>
-            <p>
-              {' '}
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-              dolor sapien, vulputate eu diam at, condimentum hendrerit tellus.
-              Nam facilisis sodales felis, pharetra pharetra lectus auctor sed.
-              Ut venenatis mauris vel libero pretium, et pretium ligula
-              faucibus. Morbi nibh felis, elementum a posuere et, vulputate et
-              erat. Nam venenatis. Cras laoreet mi Nulla auctor sit amet quam
-              vitae commodo. Sed risus justo, vulputate quis neque eget, dictum
-              sodales sem. In eget felis finibus, mattis magna a, efficitur ex.
-              Curabitur vitae justo consequat sapien gravida auctor a non risus.
-              Sed malesuada mauris nec orci congue, interdum efficitur urna
-              dignissim.{' '}
-            </p>
-            <h2>Vivamus cursus</h2>
-            <p>
-              elit sem, vel facilisis nulla pretium consectetur. Nunc congue.
-              Class aptent taciti sociosqu ad litora torquent per conubia
-              nostra, per inceptos himenaeos. Aliquam consectetur massa nec
-              metus condimentum, sed tincidunt enim tincidunt. Vestibulum
-              fringilla risus sit amet massa suscipit eleifend. Duis eget metus
-              cursus, suscipit ante ac, iaculis est. Donec accumsan enim sit
-              amet lorem placerat, eu dapibus ex porta. Etiam a est in leo
-              pulvinar auctor. Praesent sed vestibulum elit, consectetur egestas
-              libero.
-            </p>
+          <article className={styles.boxPost}>
+            {post.data.content.map(({ heading, body }) => (
+              <div key={heading}>
+                {heading && <h2>{heading}</h2>}
+
+                <div
+                  className={styles.postSection}
+                  // eslint-disable-next-line react/no-danger
+                  dangerouslySetInnerHTML={{ __html: RichText.asHtml(body) }}
+                />
+              </div>
+            ))}
           </article>
         </main>
       </div>
@@ -93,16 +109,33 @@ export default function Post(): JSX.Element {
   );
 }
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
 
-//   // TODO
-// };
+  const posts = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts'),
+  ]);
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+  const paths = posts.results.map(post => {
+    return { params: { slug: post.uid } };
+  });
 
-//   // TODO
-// };
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+
+  const prismic = getPrismicClient();
+
+  const post = await prismic.getByUID('posts', String(slug), {});
+
+  return {
+    props: {
+      post,
+    },
+  };
+};
